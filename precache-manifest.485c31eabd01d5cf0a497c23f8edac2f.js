594788288a71d2358d09ec544e777cb835aa2e4d
natya2203 <nastya_2203@ukr.net>
2019-12-05 13:42:08 +0200

    Updates

---
+++ precache-manifest.485c31eabd01d5cf0a497c23f8edac2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d19fc69fc31b1daae0e301695fdaa9",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "3e97818547486ee93177",
+    "url": "/react_static-list-of-posts/static/css/main.907ac2dc.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "3e97818547486ee93177",
+    "url": "/react_static-list-of-posts/static/js/main.d028dfdf.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
